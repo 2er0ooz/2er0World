47b128f036bf1e40a1e1de71e73859a860a4f56a
2er0ooz <2er0.oz1228@gmail.com>
2024-08-27 11:16:17 +0900

    feat: add webview

---
+++ app/src/main/java/com/opage/zeroworld/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.opage.zeroworld
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.google.com")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
